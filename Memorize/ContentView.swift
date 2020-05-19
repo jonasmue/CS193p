af85c3808697c5a072988ba0e4daba59d2bec314
Jonas Müller <jonas.mueller@compitencies.com>
2020-05-19 16:23:02 +0200

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Jonas Müller on 19.05.20.
+//  Copyright © 2020 compitencies. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
