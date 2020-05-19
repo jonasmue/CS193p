9492885140e89c8de4943c595dcca3d7253a12de
Jonas Müller <jonas.mueller@compitencies.com>
2020-05-19 17:59:42 +0200

    Initial Commit, two

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Jonas Müller on 19.05.20.
+//  Copyright © 2020 compitencies. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var game: MemoryGame<String> = createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["🤓", "🤪", "🤨", "🥳", "🧐"]
+        return MemoryGame<String>(numberOfPairsOfCards: Int.random(in: 2...5)) { pairIndex in emojis[pairIndex % emojis.count] }
+    }
+    
+    // MARK: - Accessors
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        game.cards
+    }
+    
+    // MARK: - Intents
+    
+    func choose(card: MemoryGame<String>.Card) {
+        game.choose(card)
+    }
+}
