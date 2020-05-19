959ee99a8588597210ee1b9b53a76830e9d9e272
Jonas Müller <jonas.mueller@compitencies.com>
2020-05-19 17:59:29 +0200

    Initial Commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojiGame: EmojiMemoryGame
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            ForEach(emojiGame.cards) { card in
+                CardView(card: card).onTapGesture {
+                    self.emojiGame.choose(card: card)
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(Color.blue)
+        .font(emojiGame.cards.count == 10 ? Font.body: Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white).aspectRatio(2.0/3.0, contentMode: .fit)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0).aspectRatio(2.0/3.0, contentMode: .fit)
+                Text(card.content)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill().aspectRatio(2.0/3.0, contentMode: .fit)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(emojiGame: EmojiMemoryGame())
     }
 }
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,34 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Jonas Müller on 19.05.20.
+//  Copyright © 2020 compitencies. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(id: pairIndex * 2, content: content))
+            cards.append(Card(id: pairIndex * 2 + 1, content: content))
+        }
+        cards.shuffle()
+    }
+    
+    func choose(_ card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    struct Card: Identifiable {
+        var id: Int
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
+        var content: CardContent
+    }
+}
