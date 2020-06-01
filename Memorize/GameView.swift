2efe72b298ecffc31a588d02a71756eb6637e082
Jonas Müller <jonas.mueller@compitencies.com>
2020-06-01 13:55:56 +0200

    Better display on iPad

--- Memorize/GameView.swift
+++ Memorize/GameView.swift
@@ -31,7 +31,7 @@ struct GameView: View {
                     }
                 )
             }.padding(.horizontal)
-        }
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
     
     
